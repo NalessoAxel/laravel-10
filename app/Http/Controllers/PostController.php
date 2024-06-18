c4c5c7973463e4d6d4378dd288385c65c193f46b
NalessoAxel <axelnalesso@gmail.com>
2024-06-18 08:54:43 +0200

    Add input error in client side and old helper

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\BlogFilterRequest;
+use App\Http\Requests\PostCreateRequest;
 use App\Models\Post;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
 
-    public function store(Request $request)
+    public function create(): View
     {
-        dd($request->all());
+
+        return view('blog.create');
+    }
+
+    public function store(PostCreateRequest $request)
+    {
+        $post = Post::Create($request->validated());
+        return redirect()->route('blog.show', ['slug' => $post->slug, 'post' => $post->id])->with('success', "Article sauvegardé");
     }
 
     public function index(BlogFilterRequest $request): View
@@ -35,10 +44,5 @@ class PostController extends Controller
         return view('blog.show', [
             'post' => $post,
         ]);
-    }
-
-    public function create(): View
-    {
-        return view('blog.create');
     }
 }
--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -5,12 +5,24 @@
 @section('content')
     <form action="" method="post" class="flex flex-col justify-center max-w-xl gap-3 ">
         @csrf
-        <input type="text" nom='title' value="article demo" class="p-2 border border-black rounded-md" />
+        <div>
+            <label class="text-lg">Title:</label>
+            <input type="text" name='title' value='{{ old('title', 'Mon titre') }}'
+                class="p-2 border border-black rounded-md w-min " />
+            @error('title')
+                <p class="text-red-600"> {{ $message }}</p>
+            @enderror
+        </div>
 
-        <textarea class="p-2 border border-black rounded-md" name='content' value='contenue demo'></textarea>
+        <div class="w-full">
+            <label class="text-lg">Content:</label>
+            <textarea class="p-2 border border-black rounded-md" name='content' value='{{ old('content', 'Contenu demo') }}'></textarea>
+            @error('content')
+                <p class="text-red-600"> {{ $message }}</p>
+            @enderror
+        </div>
 
         <button
             class="flex  justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enregistrer</button>
     </form>
 @endsection
-≈
